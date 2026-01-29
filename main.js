58d07418283fe54778589ccd12bf51efe740f63d
Gautam-Sah <mailtogautamsah@gmail.com>
2026-01-29 13:25:11 +0530

    learning

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import puppeteer from "puppeteer"
+const browser = await puppeteer.launch({
+  headless: false,
+  //   ignoreHTTPSErrors: true,
+  //   args: ["--ignore-certificate-errors"],
+})
 
-const browser = await puppeteer.launch()
 const page = await browser.newPage()
-
-await browser.close()
-
-browser.disconnect()
+// await page.goto("https://student.bmsit.ac.in/parents/index.php")
+await page.goto("https://www.youtube.com")
